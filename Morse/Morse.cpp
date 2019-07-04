3e62492544fe989d6f75306858c8d4f1d26cc802
Deaconxx <52459956+Deaconxx@users.noreply.github.com>
2019-07-04 10:04:51 +0800

    Update Morse.cpp

--- Morse/Morse.cpp
+++ Morse/Morse.cpp
@@ -77,32 +77,32 @@ void Morse::morselit(String n)
     {
         switch(n[j])
         {
-          case'A':case'a':dot();dash();break;
-          case'B':case'b':dash();dot();dot();dot();break;
-          case'C':case'c':dash();dot();dash();dot();break;
-          case'D':case'd':dash();dot();dot();break;
-          case'E':case'e':dot();break;
-          case'F':case'f':dot();dot();dash();dot();break;
-          case'G':case'g':dash();dash();dot();break;
-          case'H':case'h':dot();dot();dot();dot();break;
-          case'I':case'i':dot();dot();break;
-          case'J':case'j':dot();dash();dash();dash();break;
-          case'K':case'k':dash();dot();dash();break;
-          case'L':case'l':dot();dash();dot();dot();break;
-          case'M':case'm':dash();dash();break;
-          case'N':case'n':dash();dot();break;
-          case'O':case'o':dash();dash();dash();break;
-          case'P':case'p':dot();dash();dash();dot();break;
-          case'Q':case'q':dash();dash();dot();dash();break;
-          case'R':case'r':dot();dash();dot();break;
-          case'S':case's':dot();dot();dot();break;
-          case'T':case't':dash();break;
-          case'U':case'u':dot();dot();dash();break;
-          case'V':case'v':dot();dot();dot();dash();break;
-          case'W':case'w':dot();dash();dash();break;
-          case'X':case'x':dash();dot();dot();dash();break;
-          case'Y':case'y':dash();dot();dash();dash();break;
-          case'Z':case'z':dash();dash();dot();dot();break;
+          case'A':dot();dash();break;
+          case'B':dash();dot();dot();dot();break;
+          case'C':dash();dot();dash();dot();break;
+          case'D':dash();dot();dot();break;
+          case'E':dot();break;
+          case'F':dot();dot();dash();dot();break;
+          case'G':dash();dash();dot();break;
+          case'H':dot();dot();dot();dot();break;
+          case'I':dot();dot();break;
+          case'J':dot();dash();dash();dash();break;
+          case'K':dash();dot();dash();break;
+          case'L':dot();dash();dot();dot();break;
+          case'M':dash();dash();break;
+          case'N':dash();dot();break;
+          case'O':dash();dash();dash();break;
+          case'P':dot();dash();dash();dot();break;
+          case'Q':dash();dash();dot();dash();break;
+          case'R':dot();dash();dot();break;
+          case'S':dot();dot();dot();break;
+          case'T':dash();break;
+          case'U':dot();dot();dash();break;
+          case'V':dot();dot();dot();dash();break;
+          case'W':dot();dash();dash();break;
+          case'X':dash();dot();dot();dash();break;
+          case'Y':dash();dot();dash();dash();break;
+          case'Z':dash();dash();dot();dot();break;
           case' ':space();break;
           case'\n':enter();break;
           }
